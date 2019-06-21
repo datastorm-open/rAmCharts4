7c7979c4a3e53dd46350ac3284c73a183f4f402e
Titouan <titouan.robert@datastorm.fr>
2019-06-21 11:36:37 +0200

    add function for serie

--- R/utils2.R
+++ R/utils2.R
@@ -19,4 +19,36 @@ dataFields <- function(value = NULL, category = NULL,
     ... = ...))
 }
 
+#' Add series information
+#'
+#' @param type A \code{character} type of serie.
+#' @param stroke A \code{character} color, hexa.
+#' @param strokeWidth A \code{interger} size.
+#' @param ... Arguments defined in \url{https://www.amcharts.com/docs/v4/}.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' amCharts4(data = data.frame(tag = c("My", "Best", "Word"),
+#'                             weight = c(100, 200, 300)),
+#'           series = series(type = "WordCloudSeries",
+#'                           dataFields = dataFields(
+#'                             word = "tag",
+#'                             value = "weight")),
+#'           type = "WordCloud")
+#' }
+#'
+#' @return A \code{list}.
+#' @export
+series <- function(type = NULL, stroke = NULL,
+               strokeWidth = NULL,
+               ...){
+  list(dropNulls(list(
+    "type" = type,
+    "stroke" = stroke,
+    "strokeWidth" = strokeWidth,
+    ... = ...)))
+}
+
+
 
